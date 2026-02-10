71e526dc1a3bca9ebd6e0ee489c61b41e8f89512
Jeremy <packardjc@outlook.com>
2026-02-10 15:03:58 -0500

    api and data cleaning working. working on html/cc

--- web/home_page/models.py
+++ web/home_page/models.py
@@ -6,3 +6,15 @@ class Location(models.Model):
     state = models.CharField(blank=False, null=False)
     longitude = models.DecimalField(blank=True, null=True, decimal_places=2, max_digits=4)
     latitude = models.DecimalField(blank=True, null=True, decimal_places=2, max_digits=4)
+
+    def __str__(self):
+        return f'{self.city.title()}, {self.state.upper()}'
+
+
+class Temperature(models.Model):
+    city = models.ForeignKey(Location, on_delete=models.CASCADE)
+    temp = models.IntegerField(blank=False, null=False)
+    hour = models.DateTimeField(blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.hour} - {self.temp}'
--- web/home_page/views.py
+++ web/home_page/views.py
@@ -6,7 +6,11 @@ import requests
 from datetime import datetime, time, timedelta
 
 
-def generate_zip_url(city, state):
+###############################################################################
+# Functions
+###############################################################################
+
+def get_lat_long(city, state):
     """
     Uses the zippoptam api to get the lat/long for an city/state combo. 
     """
@@ -21,30 +25,100 @@ def generate_zip_url(city, state):
         return 181, 91
     
 
-def generate_forecast_url(long, lat):
+def get_temps(long, lat, days=7, temp_unit='fahrenheit'):
+    """
+    Uses the lat and long parameters for the api url and access open-meteo
+    for a forecast for the given amount of days and returns the json data 
+    converted to a Python dict. 
+    """
     url = (
         'https://api.open-meteo.com/v1/forecast?'
         f'latitude={lat}'
         f'&longitude={long}'
         '&hourly=temperature_2m'
         '&timezone=auto'
-        '&forecast_days=7'
-        '&temperature_unit=fahrenheit'
+        f'&forecast_days={days}'
+        f'&temperature_unit={temp_unit}'
     )
     response = requests.get(url)
     if response.status_code == 200:
-        data = response.json()
-        temp_list = []
-        num_hours = len(data['hourly']['time'])
-        for i in range(num_hours):
-            temp_list.append({
-                'hour': data['hourly']['time'][i],
-                'temp': round(data['hourly']['temperature_2m'][i])
-            })
-        return temp_list
+        return response.json()
+    else:
+        return
+    
+
+def save_forecast_data(location):
+    """
+    Processes the json data into Temperature objects for each hour and saves 
+    it to the database. 
+    """
+    data = get_temps(location.longitude, location.latitude)
+    for i in range(len(data['hourly']['temperature_2m'])):
+        hour = datetime.strptime(data['hourly']['time'][i], '%Y-%m-%dT%H:%M')
+        temp = round(data['hourly']['temperature_2m'][i])
+        temp_obj = Temperature.objects.create(
+            city=location,
+            hour=hour,
+            temp=temp
+        )
+        temp_obj.save()
+
+
+def forecast_exists(location):
+    """
+    Checks whether a forecast already exists in the database for the given 
+    start day. If so it uses that instead of making a new api request. If 
+    not it calls the save_forecast_data function. If the day exists, but is 
+    not the first entry it will delete the old data for that city and make 
+    a new request.
+    """
+    today = datetime.combine(datetime.today(), time(0, 0, 0))
+    temps = Temperature.objects.filter(city=location).order_by('hour')
+    if temps.exists(): 
+        if temps[0].hour == today:
+            return True
+        else:
+            for temp in temps:
+                temp.delete()
+            return False
     else:
         return False
+    
 
+def format_temps_context(location):
+    """
+    Gets the forecast data from the table and formats the context dictionary 
+    correctly for use in the template render.
+    """
+    temps = Temperature.objects.filter(city=location).order_by('hour')
+    temp_dict = {
+        'location': f'{location.city.title()}, {location.state.upper()}',
+        'days': [],
+        }
+    for i in range(0, len(temps), 24):
+        hours = temps[i:i+24]
+        day_dict = {
+            'day': str(hours[0].hour.month) + '/' + str(hours[0].hour.day),
+            'temps': [],
+            'high': hours[0].temp,
+            'low': hours[0].temp
+        }
+        for hour in hours:
+            day_dict['temps'].append({
+                'hour': hour.hour.strftime('%I:%M %p').lstrip('0'),
+                'temp': hour.temp,
+            })
+            if hour.temp > day_dict['high']:
+                day_dict['high'] = hour.temp
+            if hour.temp < day_dict['low']:
+                day_dict['low'] = hour.temp
+        temp_dict['days'].append(day_dict)
+    return temp_dict
+
+
+###############################################################################
+# Views
+###############################################################################
 
 def index(request):
     context = {}
@@ -53,75 +127,31 @@ def index(request):
     if 'error_message' in request.session:
         context['error_message'] = request.session['error_message']
 
-    # Get the most recently added 4 locations
-    locations = Location.objects.all().order_by('-id')[:4]
-    context['locations'] = locations
+    # Get the saved locations from the database
+    locations = Location.objects.all().order_by('-id')
+    context['locations'] = []
 
-    # Get the forecast for those locations
-    context['temps'] = []
-    current_date = datetime.today()
-    current_hour = datetime.combine(current_date, time(0, 0, 0))
-    one_hour = timedelta(hours=1)
-
+    # Get the forecast starting with today.
     for location in locations:
-        # Create the forecast dictionary
-        temp_dict = {}
-        temp_dict['display_name'] = f'{location.city}, {location.state}'
-        temp_dict['forecast'] = []
-        days = []
+        if not forecast_exists(location):
+            save_forecast_data(location)
+        context['locations'].append(format_temps_context(location))
 
-        # Start with today
-        date = current_hour.date()
-        day = f'{date.month}/{date.day}'
-        days.append(day)
-        day_dict = {
-            'day': day,
-            'hours': []
-        }
-
-        # Get the forecast data using the open-meteo api  
-        temps = generate_forecast_url(location.longitude, location.latitude)
-        if not temps:
-            return HttpResponse(f'something went wrong for {location.city}, {location.state}')
-        
-        # print(temps)
-        # print(len(temps))
-
-        for temp in temps:
-            date = current_hour.date()
-            day = f'{date.month}/{date.day}'
-            if day not in days:
-                # Reset day
-                temp_dict['forecast'].append(day_dict)
-                days.append(day)
-                day_dict = {}
-                day_dict['day'] = day
-                day_dict['hours'] = []
-            day_dict['hours'].append({
-                    'hour': current_hour.time(),
-                    'temp': temp
-            })
-            current_hour = current_hour + one_hour
-
-        context['temps'].append(temp_dict)
-        
-        # print(context['temps'])
-
-    # return HttpResponse(data)
     return render(request, 'home_page/index.html', context)
 
 
 def search(request):
     """
     Uses Zippopotam to get the lat/long for the city/state combo and then creates
-    and saves a location object to the database. 
+    and saves a location object to the database. If there are already four 
+    locations in the database then the oldes location will be deleted. 
     """
     if request.method == 'POST':
         # Get the city and state from the form.
         city = request.POST.get('city').lower().strip()
         state = request.POST.get('state').lower().strip()
         # Use Zippopotam to get the Lat/Long
-        long, lat = generate_zip_url(city, state)
+        long, lat = get_lat_long(city, state)
         # Verify that it is a legitimate lat/long
         if long > 180 and lat == 90:
             request.session['error_message'] = 'Unable to find location.'
@@ -138,6 +168,7 @@ def search(request):
             locations = Location.objects.all().order_by('id')
             if len(locations) > 4:
                 locations[0].delete()
+            # Resets the error message for the sesson
             if 'error_message' in request.session:
                 del request.session['error_message']
             request.session['error_message'] = None
@@ -146,7 +177,7 @@ def search(request):
 
 def delete_location(request):
     """
-    Allows the user to delete a location by clicked the button.
+    Allows the user to delete a location by clicking the button.
     """
     if request.method == 'POST':
         city = request.POST.get('city_name')
