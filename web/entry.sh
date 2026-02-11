12328666ae85817aabfb77ec5a81950c41f011f7
Jeremy <packardjc@outlook.com>
2026-02-11 12:57:36 -0500

    .

--- web/entry.sh
+++ web/entry.sh
@@ -11,8 +11,8 @@ VOLUMES=(
     "/app/static"
 )
 
-chown -R www-data:www-data /app/static || true
-chmod -R 755 /app/static || true
+sudo chown -R www-data:www-data /app/static || true
+sudo chmod -R 755 /app/static || true
 
 for dir in "${VOLUMES[@]}"; do
     mkdir -p "$dir"
