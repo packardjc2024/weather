660e76a9a1fe986ae961bede5364cc62c7017947
Jeremy <packardjc@outlook.com>
2026-02-11 13:06:03 -0500

    still working on favicon

--- web/project/settings.py
+++ web/project/settings.py
@@ -157,6 +157,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+if not DEBUG:
+    STATICFILES_DIRS = []
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
